f921ea73ccdbdb95f5a764bc05b1f66d6823983e
Michael Buranov <916310@rambler.ru>
2024-01-08 20:32:49 +0400

    12 Navbar. Шаблоны для разработки. Первый UI Kit компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './styles/index.scss'
-import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
@@ -12,11 +12,10 @@ const App = () => {
 
     return (
         <div className={classNames("app", {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>TOGGLE</button>
+
         </div>
     );
 };
