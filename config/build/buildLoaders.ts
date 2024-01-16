9eede935a6d3ce6f70892ce8096f1841c2ecfcc9
Michael Buranov <916310@rambler.ru>
2024-01-16 12:09:52 +0400

    17 Babel. Extract plugin [optional]

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,26 @@ export function BuildLoaders(options: BuildOptions): RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ["ru", "en"],
+                            keyAsDefaultValue: true,
+                        }
+                    ],
+                ]
+            }
+        }
+    };
+
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
         use: [
@@ -47,6 +67,7 @@ export function BuildLoaders(options: BuildOptions): RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoaders
     ]
