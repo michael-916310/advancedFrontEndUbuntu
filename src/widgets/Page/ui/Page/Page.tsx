8894ed9be80f03c7ba9bc9c9cab623ff8bd27939
michael-916310 <mburanov@alfabank.ru>
2025-12-18 12:04:54 +0300

    108 Lint staged и pre commit хуки

--- src/widgets/Page/ui/Page/Page.tsx
+++ src/widgets/Page/ui/Page/Page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 import { MutableRefObject, UIEvent, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
