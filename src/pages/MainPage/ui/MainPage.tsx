f0e0846b5df72d6e81901130d6ca829874b6ffdf
michael-916310 <mburanov@alfabank.ru>
2026-01-14 18:07:05 +0300

    change main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
-import { Counter } from '@/entities/Counter';
 
 function MainPage() {
     const { t } = useTranslation();
 
     return (
         <Page data-testid="MainPage">
-            <Counter />
+            {/* <Counter /> */}
             {t('Главная страница')}
         </Page>
     );
