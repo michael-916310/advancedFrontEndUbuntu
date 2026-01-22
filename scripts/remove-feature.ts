d3a2677a6447aaa3b2898fc4dadcc62418e01baf
michael-916310 <mburanov@alfabank.ru>
2026-01-22 08:24:25 +0300

    fix remove-feature.ts

--- scripts/remove-feature.ts
+++ scripts/remove-feature.ts
@@ -160,15 +160,16 @@ const replaceComponent = (node: Node) => {
 };
 
 sourceFiles.forEach((sourceFile) => {
+    // eslint-disable-next-line consistent-return
     sourceFile.forEachDescendant((node) => {
         if (node.isKind(SyntaxKind.CallExpression) && isToggleFunction(node)) {
-            replaceToggleFunction(node);
+            return replaceToggleFunction(node);
         }
         if (
             node.isKind(SyntaxKind.JsxSelfClosingElement) &&
             isToggleComponent(node)
         ) {
-            replaceComponent(node);
+            return replaceComponent(node);
         }
     });
 });
